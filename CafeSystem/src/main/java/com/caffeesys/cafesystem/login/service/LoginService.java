975e7a0bbd0e34f1e67f8a478d2b152bafa7971b
Jeon-Yoo-jin <didchthsu@daum.net>
2017-11-16 12:48:20 +0900

    레이아웃수정

---
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/login/service/LoginService.java
@@ -0,0 +1,11 @@
+package com.caffeesys.cafesystem.login.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+	@Autowired
+	private LoginDao loginDao;
+	
+}
