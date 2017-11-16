5cb4b8feb3128a8300f68e123a73a48a41742356
SONG JEONGHUN <diminish13@gmail.com>
2017-11-16 17:16:08 +0900

    Customer Claim

--- CafeSystem/src/main/java/com/caffeesys/cafesystem/board/service/ClaimDao.java
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/board/service/ClaimDao.java
@@ -1,5 +1,7 @@
 package com.caffeesys.cafesystem.board.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,6 +13,11 @@ public class ClaimDao {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	
+	//전체 리스트 셀렉트
+	public List<Claim> selectAllClaim(){
+		return sqlSessionTemplate.selectList(NS + "selectAllClaim");
+	}
+	//클레임 게시글 등록
 	public int insertClaim(Claim claim) {
 		return sqlSessionTemplate.insert(NS + "insertClaim", claim);
 	}
