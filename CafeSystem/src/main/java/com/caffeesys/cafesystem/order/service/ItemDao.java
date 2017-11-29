a2e8f8053f792218f49b48377091d300ff5408fa
Tak-hyeonsu <tag0066@naver.com>
2017-11-29 11:29:53 +0900

    item 리스트 컨트롤러부터

--- CafeSystem/src/main/java/com/caffeesys/cafesystem/order/service/ItemDao.java
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/order/service/ItemDao.java
@@ -1,8 +1,28 @@
 package com.caffeesys.cafesystem.order.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ItemDao {
-
+	
+private final String NS = "com.caffeesys.cafesystem.order.service.ItemMapper.";
+	
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	// 발주품목리스트 및 조회 상세전
+	public List<ItemVO> selectItemList(Map<String, String> map) {
+		System.out.println("ItemDao.java / selectItemList method 확인");
+	    return sqlSessionTemplate.selectList(NS+"selectItemList", map);
+	}
+	// 발주품목리스트 및 조회 상세전 행의수
+	public int selectItemCount(Map<String, String> map) {
+		System.out.println("ItemDao.java / selectItemCount method 확인");
+		return sqlSessionTemplate.selectOne(NS+"selectItemCount", map);
+	}
 }
--- CafeSystem/src/main/java/com/caffeesys/cafesystem/order/service/ItemService.java
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/order/service/ItemService.java
@@ -1,8 +1,40 @@
 package com.caffeesys.cafesystem.order.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import com.caffeesys.cafesystem.account.controller.PasingService;
 
 @Service
 public class ItemService {
-
+	
+	@Autowired
+	PasingService pasingService; // 페이징 및 검색(account.controller폴더에)
+	@Autowired
+	private ItemDao itemDao;
+	
+	// 발주품목리스트 및 조회 상세전
+	public void selectItemList(Model model, String searchOption, String keyword, int currentPage) {
+		System.out.println("ItemService.java / selectItemList method 확인");
+		System.out.println("ItemService.java / selectItemList Param model :" + model);
+		System.out.println("ItemService.java / selectItemList Param searchOption :" + searchOption);
+		System.out.println("ItemService.java / selectItemList Param keyword :" + keyword);
+		System.out.println("ItemService.java / selectItemList Param currentPage :" + currentPage);
+		Map<String, String> map;
+		if(searchOption != "") {
+			map = new HashMap<String, String>();
+			map.put("searchOption", searchOption);
+			map.put("keyword",keyword);			
+		}else {
+			map = null;
+		}
+		System.out.println("ItemService.java"+map);
+		map = pasingService.paging(model, currentPage, 10, itemDao.selectItemCount(map), map);
+		model.addAttribute("itemList", itemDao.selectItemList(map));
+		model.addAttribute("itemCount", itemDao.selectItemCount(map));
+	}
 }
