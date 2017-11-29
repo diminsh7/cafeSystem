ea84f1224ea1ec38c375458abf604881b9f207c2
Tak-hyeonsu <tag0066@naver.com>
2017-11-29 11:05:08 +0900

    item class생성 및 준비단계

---
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/order/controller/ItemController.java
@@ -0,0 +1,8 @@
+package com.caffeesys.cafesystem.order.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ItemController {
+
+}
---
+++ CafeSystem/src/main/java/com/caffeesys/cafesystem/order/service/ItemDao.java
@@ -0,0 +1,8 @@
+package com.caffeesys.cafesystem.order.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDao {
+
+}
